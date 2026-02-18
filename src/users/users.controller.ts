333880b6c5596e2e1c888d27d51bd6f6a1eba5ab
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2026-02-18 17:05:16 +0500

    DTO Validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,8 +7,12 @@ import {
     Patch,
     Post,
     Query,
+    ParseIntPipe,
+    ValidationPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdateUserDTO } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -20,37 +24,29 @@ export class UsersController {
     }
 
     @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.usersService.findOne(+id);
+    findOne(@Param('id', ParseIntPipe) id: number) {
+        return this.usersService.findOne(id);
     }
 
     @Post()
     create(
-        @Body()
-        user: {
-            name: string;
-            email: string;
-            role: 'INTERN' | 'ENGINEER' | 'ADMIN';
-        },
+        @Body(ValidationPipe)
+        createUserDTO: CreateUserDTO,
     ) {
-        return this.usersService.create(user);
+        return this.usersService.create(createUserDTO);
     }
 
     @Patch(':id')
     update(
-        @Param('id') id: string,
-        @Body()
-        userUpdate: {
-            name?: string;
-            email?: string;
-            role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
-        },
+        @Param('id', ParseIntPipe) id: number,
+        @Body(ValidationPipe)
+        updateUserDTO: UpdateUserDTO,
     ) {
-        return this.usersService.update(+id, userUpdate);
+        return this.usersService.update(id, updateUserDTO);
     }
 
     @Delete(':id')
-    delete(@Param('id') id: string) {
-        return this.usersService.delete(+id);
+    delete(@Param('id', ParseIntPipe) id: number) {
+        return this.usersService.delete(id);
     }
 }
